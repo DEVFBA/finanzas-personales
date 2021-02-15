bcff22e30fa193e827a01f3c0cfc8441f28c62a0
Angel Gutierrez <angenrique.gutierrez@gmail.com>
2021-02-14 22:29:39 -0600

    Retrieve Historical Data

--- src/components/FinancialMarkets.js
+++ src/components/FinancialMarkets.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {
+    useState
+} from 'react';
 import {
     Container,
     Row,
-    Col
+    Col,
+    Form,
+    Button
 } from 'react-bootstrap';
 
 import PageConstruction from './PageConstruction';
@@ -10,8 +14,43 @@ import PageConstruction from './PageConstruction';
 import '../styles/FinancialMarkets.css';
 
 const FinancialMarkets = () => {
+
+    const [ticker, setTicker]           = useState('');
+    const [dateFrom, setDateFrom]       = useState('');
+    const [dateThru, setDateThru]       = useState('');
+    const [tickerData, setTickerData]   = useState([]);
+
+    const onChangeTicker = (event) => {
+        setTicker(event.target.value.trim());
+    }
+
+    const onChangeDateFrom = (event) => {
+        setDateFrom(event.target.value);
+    }
+
+    const onChangeDateThru = (event) => {
+        setDateThru(event.target.value);
+    }
+
+    const onSubmitTicker = (event) => {
+
+        const token                     = process.env.REACT_APP_MARKETSTACK_TOKEN;
+
+        event.preventDefault();
+
+        fetch(`http://api.marketstack.com/v1/eod?access_key=${token}&symbols=${ticker}&date_from=${dateFrom}&date_to=${dateThru}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setTickerData(data.data);
+            console.log(tickerData);
+        })
+    }
+
     return(
         <Container
+            fluid
             className='financial-markets'
         >
             <Row>
@@ -19,6 +58,58 @@ const FinancialMarkets = () => {
             </Row>
             <Row>
                 <PageConstruction />
+            </Row>
+            <Row>
+                <Form
+                    onSubmit = { onSubmitTicker }
+                >
+                    <Form.Row>
+                        <Col
+                            md = { 3 }
+                        >
+                            <Form.Label>
+                                Ticker
+                            </Form.Label>
+                            <Form.Control 
+                                type            = 'text'
+                                placeholder     = 'Ticker'
+                                onChange        = { onChangeTicker }    
+                            />
+                        </Col>
+                        <Col
+                            md = { 3 }
+                        >
+                            <Form.Label>
+                                Fecha Desde
+                            </Form.Label>
+                            <Form.Control 
+                                type        ='date' 
+                                onChange    = { onChangeDateFrom }   
+                            />
+                        </Col>
+                        <Col
+                            md = { 3 }
+                        >
+                            <Form.Label>
+                                Fecha Hasta
+                            </Form.Label>
+                            <Form.Control 
+                                type        = 'date'  
+                                onChange    = { onChangeDateThru }  
+                            />
+                        </Col>
+                        <Col
+                            md = { 3 }
+                        >
+                            <Button
+                                variant = 'outline-light'
+                                type    = 'submit'
+                            >
+                                Buscar
+                            </Button>
+                        </Col>
+                    </Form.Row>
+                </Form>
             </Row>
         </Container>
     );
