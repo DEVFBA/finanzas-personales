844a3a32342a854562ca54d7d3eb3a19901820ce
Angel Gutierrez <angenrique.gutierrez@gmail.com>
2021-02-15 01:39:24 -0600

    API Marketstack implementation

--- src/components/FinancialMarkets.js
+++ src/components/FinancialMarkets.js
@@ -8,6 +8,9 @@ import {
     Form,
     Button
 } from 'react-bootstrap';
+import {
+    Line
+} from 'react-chartjs-2';
 
 import PageConstruction from './PageConstruction';
 
@@ -18,7 +21,7 @@ const FinancialMarkets = () => {
     const [ticker, setTicker]           = useState('');
     const [dateFrom, setDateFrom]       = useState('');
     const [dateThru, setDateThru]       = useState('');
-    const [tickerData, setTickerData]   = useState([]);
+    const [tickerData, setTickerData]   = useState([{}]);
 
     const onChangeTicker = (event) => {
         setTicker(event.target.value.trim());
@@ -44,9 +47,22 @@ const FinancialMarkets = () => {
         })
         .then((data) => {
             setTickerData(data.data);
-            console.log(tickerData);
-        })
+            //console.log('Data', data.data);
+            //console.log('Data for State', ...data.data);
+            //console.log('Ticker 1', tickerData);
+        });
     }
+
+    console.log('Ticker Data Out', tickerData);
+
+    const dates = tickerData.map((e) => e.date.substring(0, 10)).reverse();
+    const closes = tickerData.map((e) => e.close).reverse();
+
+    console.log('Dates', dates);
+    console.log('Closes', closes);
+    //const dates = tickerData.map((data) => data.date);
+
+
 
     return(
         <Container
@@ -110,6 +126,20 @@ const FinancialMarkets = () => {
                         </Col>
                     </Form.Row>
                 </Form>
+            </Row>
+            <Row
+                className = "mt-5"
+            >
+                <Line 
+                    data = {{
+                        labels: dates,
+                        datasets: [{
+                            label: 'Close',
+                            data: closes,
+                            backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                        }]
+                    }}
+                />
             </Row>
         </Container>
     );
